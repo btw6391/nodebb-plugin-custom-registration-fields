8a9981bbcff9a0cf12bfb66bf9f1090d3a7a07d7
Brock Webberman <btw6391@gmail.com>
2017-03-17 14:40:42 -0500

    Remove onRegisterComplete

--- library.js
+++ library.js
@@ -219,11 +219,6 @@ plugin.addToApprovalQueue = function(params, callback) {
     callback(null, {data: data, userData: userData});
 };
 
-plugin.onRegisterComplete = function(data, callback) {
-    data.referrer = 'https://http://45.55.167.216:4567/login';
-    callback(null, data);
-}
-
 function renderAdmin(req, res, next) {
 	res.render('admin/custom-registration-fields', {fields: customFields});
 }
